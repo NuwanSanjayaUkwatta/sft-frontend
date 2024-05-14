6e99e8a49654f991ea5a7629e851566e313d337e
NuwanSanjayaUkwatta <nukwaththa@gmail.com>
2024-05-15 00:48:26 +0530

    new change url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://dba6-2402-4000-2200-3d04-b9c4-5ef-7f08-123a.ngrok-free.app',
+    baseURL: 'https://3ad2-2402-4000-2200-b1ce-e08c-6cee-abbb-3f45.ngrok-free.app',
     headers: {
         'Content-Type': 'application/json',
         "ngrok-skip-browser-warning": "69420",
