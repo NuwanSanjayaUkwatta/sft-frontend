52c97f02038e5fdd35316494fc0b6b5b8027309e
NuwanSanjayaUkwatta <nukwaththa@gmail.com>
2024-05-23 23:01:48 +0530

    new url 15

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://07a2-2402-4000-2081-6651-cd3e-50b3-79ce-d760.ngrok-free.app',
+    baseURL: 'https://6509-2402-4000-2081-6651-cd3e-50b3-79ce-d760.ngrok-free.app',
     headers: {
         'Content-Type': 'application/json',
         "ngrok-skip-browser-warning": "69420",
